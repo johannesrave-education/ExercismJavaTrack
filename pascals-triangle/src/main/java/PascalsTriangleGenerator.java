460f66cc8eeb9f169d94e35f1a328fd5c55dcbca
cutmeat <johannes.rave@gmail.com>
2020-09-15 20:46:07 +0200

    PascalsTriangleGenerator solution added

--- pascals-triangle/src/main/java/PascalsTriangleGenerator.java
+++ pascals-triangle/src/main/java/PascalsTriangleGenerator.java
@@ -1,5 +1,15 @@
 class PascalsTriangleGenerator{
     public static int[][] generateTriangle(int height){
-        return null;
+        
+        int[][] triangle = new int[height][];
+        for (int row = 0; row < triangle.length; row++) {
+            triangle[row] = new int[row + 1];
+            for (int col = 0; col < triangle[row].length; col++) {
+                triangle[row][col] = (col == 0 || col == row) ? 1
+                                             : triangle[row-1][col-1] + triangle[row-1][col];
+            }
+        }
+
+        return triangle;
     }
 }
